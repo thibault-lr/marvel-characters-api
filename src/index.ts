e84d3c30088503028995302389a7922bf647cf96
thibault.leroy <thibault.le-roy@saegus.com>
2020-03-01 23:19:07 +0100

    improve server start function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 
 import Server from './server';
-import dotenv from "dotenv"
 
-dotenv.config()
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+( async () => {
+  //set up server exceptions
+  process.on("uncaughtException", (error: Error) => {
+    console.error("uncaughtException", error.stack);
+    process.exit(1);
+  });
 
-const server = new Server({host: process.env.NODE_HOST, port: process.env.NODE_PORT});
-server.initControllers()
-server.startServer()
+  process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Rejection at:", promise, "reason:", reason);
+    process.exit(1);
+  });
+
+  const server = new Server({ host: process.env.NODE_HOST, port: process.env.NODE_PORT });
+  server.initControllers()
+  try {
+    await server.startServer()
+  } catch (e) {
+    console.error("Server error : ", e.stack)
+  }
+    
+})()
