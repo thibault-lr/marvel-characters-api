d13b7d3f552702cc3e9f5c112ef6cad5f99349bf
thibault.leroy <thibault.le-roy@saegus.com>
2020-03-02 00:25:33 +0100

    add documentation for chracters

--- src/api/characters/characters.routes.ts
+++ src/api/characters/characters.routes.ts
@@ -13,11 +13,31 @@ export function charactersRoutes(server: Hapi.Server, routePrefix = "/v1"): void
     path: `${routePrefix}/characters`,
     options: {
       handler: charController.getCharacters,
+      description: 'Get characters',
+      notes: 'Returns a list of characters',
       validate: {
         query: {
           offset: Joi.number()
         }
-      }
+      },
+      response: {
+        status: {
+          200: Joi.object({
+            total: Joi.number(),
+            count: Joi.number(),
+            offset: Joi.number(),
+            limit: Joi.number(),
+            characters: Joi.array().items(Joi.object().keys({
+              id: Joi.number(),
+              name: Joi.string(),
+              image: Joi.string()
+            }))
+          }).label('Result'),
+          404: Joi.any()
+        }
+      },
+      tags: ['api'],
+      auth: false
     }
   })
 }
